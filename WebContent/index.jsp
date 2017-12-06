279c2c28607a04044bf60f680eed918295b537be
lovejesus27 <33417071+lovejesus27@users.noreply.github.com>
2017-12-06 21:06:45 +0900

    1206, 9:06

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -11,5 +11,9 @@
 	<!-- category by kj -->
 	<a href ="./category/categoryList.category">category</a>
 	<a href ="./meet/meetList.meet">모임</a>
+	
+	<!-- notice, qna by suin -->
+	<a href="./notice/noticeList.notice">Notice List</a>
+	<a href="./qna/qnaList.qna">Qna List</a>
 </body>
 </html>
--- src/com/ede/controller/NoticeController.java
+++ src/com/ede/controller/NoticeController.java
@@ -1,11 +1,20 @@
 package com.ede.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.Action;
 
 /**
  * Servlet implementation class NoticeController
@@ -13,21 +22,60 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/NoticeController")
 public class NoticeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public NoticeController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+    
+	private Map<String, Object> command;
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	
+	public NoticeController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		command = new HashMap<>();
+		String filePath = config.getServletContext().getRealPath("WEB-INF/properties");
+		String fileName = config.getInitParameter("properties");
+		File file = new File(filePath, fileName);
+		FileInputStream fi = null;
+		Properties prop = new Properties();
+		
+		try {
+			fi = new FileInputStream(file);
+			prop.load(fi);
+			Iterator<Object> it = prop.keySet().iterator();
+			while(it.hasNext()) {
+				String key = (String)it.next();
+				String value = (String)prop.get(key);
+				Class ins = Class.forName(value);
+				Object obj = ins.newInstance();
+				command.put(key, obj);
+			}
+		}catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}finally {
+			try {
+				fi.close();
+			}catch (IOException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	
+	
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String path = request.getServletPath();
+		Action
 	}
 
 	/**
