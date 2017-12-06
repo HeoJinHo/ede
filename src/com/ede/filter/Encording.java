ec86fcdf1c37f77b3a70320418bbd95af8fe0327
lovejesus27 <33417071+lovejesus27@users.noreply.github.com>
2017-12-06 16:06:56 +0900

    11

--- src/com/ede/filter/Encording.java
+++ src/com/ede/filter/Encording.java
@@ -12,7 +12,7 @@ import javax.servlet.annotation.WebFilter;
 /**
  * Servlet Filter implementation class Encording
  */
-@WebFilter("/Encording")
+
 public class Encording implements Filter {
 	private String enc;
 
