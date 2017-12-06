b48125bfb0d7547ec66e8e3b897ee0d78bc3f392
jungwonjang <jangmung1234@naver.com>
2017-12-06 16:58:59 +0900

    a

---
+++ src/com/ede/meet/MeetService.java
@@ -0,0 +1,19 @@
+package com.ede.meet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ede.action.Action;
+import com.ede.action.ActionFoward;
+
+public class MeetService implements Action {
+
+	@Override
+	public ActionFoward doProcess(HttpServletRequest request, HttpServletResponse response) {
+		ActionFoward actionFoward = new ActionFoward();
+		
+		
+		return actionFoward;
+	}
+
+}
