67ab9bc0bed07bc00c319e24de48cbbbed5e8145
truly412 <truly412@naver.com>
2017-12-06 21:28:19 +0900

    1206/9:28 kj
    category

--- src/com/ede/category/CategoryListService.java
+++ src/com/ede/category/CategoryListService.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.ede.action.Action;
 import com.ede.action.ActionFoward;
 
-public class CategoryService implements Action{
+public class CategoryListService implements Action{
 	@Override
 	public ActionFoward doProcess(HttpServletRequest request, HttpServletResponse response) {
 		ActionFoward actionFoward = new ActionFoward();
@@ -16,17 +16,14 @@ public class CategoryService implements Action{
 		try {
 			List<CategoryDTO> ar = categoryDAO.selectList();
 			System.out.println((ar.get(0)).getBrand());
+			System.out.println((ar.get(0)).getCapacity());
 			request.setAttribute("list", ar);
 		} catch (Exception e) {
 		}
 		actionFoward.setCheck(true);
 		actionFoward.setPath("../WEB-INF/view/category/categoryList.jsp");
-		
-		
+
+
 		return actionFoward;
 	}
-	
-	
-	
-
 }
