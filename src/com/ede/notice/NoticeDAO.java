11a84c04d3c1cac4669e8ace7a058ef08b156b34
lovejesus27 <33417071+lovejesus27@users.noreply.github.com>
2017-12-06 20:06:15 +0900

    1206, 8:06

--- src/com/ede/notice/NoticeDAO.java
+++ src/com/ede/notice/NoticeDAO.java
@@ -3,6 +3,7 @@ package com.ede.notice;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.ede.board.BoardDAO;
@@ -45,31 +46,86 @@ public class NoticeDAO implements BoardDAO {
 		st.setString(1, boardDTO.getNot_title());
 		st.setString(2, boardDTO.getContents());
 		st.setInt(3, boardDTO.getNot_num());
-		return 0;
+		int result = st.executeUpdate();
+		DBConnector.disConnect(st, con);
+		
+		return result;
 	}
 
 	public int delete(int num) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		Connection con = DBConnector.getConnect();
+		String sql = "delete notice where num=?";
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setInt(1, num);
+		int result = st.executeUpdate();
+		DBConnector.disConnect(st, con);
+		
+		return result;
 	}
 
 	public BoardDTO selectOne(int num) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		Connection con = DBConnector.getConnect();
+		String sql = "select * from notice where num=?";
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setInt(1, num);
+		ResultSet rs = st.executeQuery();
+		NoticeDTO noticeDTO = null;
+		if(rs.next()) {
+			noticeDTO = new NoticeDTO();
+			noticeDTO.setNot_num(rs.getInt("not_num"));
+			noticeDTO.setNot_title(rs.getString("not_title"));
+			noticeDTO.setWriter(rs.getString("writer"));
+			noticeDTO.setContents(rs.getString("contents"));
+			noticeDTO.setReg_date(rs.getDate("reg_date"));
+			noticeDTO.setHit(rs.getInt("hit"));
+		}
+		DBConnector.disConnect(rs, st, con);
+		
+		return noticeDTO;
 	}
 
 	public List<BoardDTO> selectList(MakeRow makeRow) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		List<BoardDTO> ar = new ArrayList<BoardDTO>();
+		Connection con = DBConnector.getConnect();
+		String sql ="select * from" 
+				+"(select rownum R, N.* from"
+				+"(select * from notice where "+makeRow.getKind()+" like ? order by num desc) N) "
+				+"where R between ? and ?";
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setString(1, "%"+makeRow.getSearch()+"%");
+		st.setInt(2, makeRow.getStartRow());
+		st.setInt(3, makeRow.getLastRow());
+		ResultSet rs = st.executeQuery();
+		while(rs.next()) {
+			NoticeDTO noticeDTO = new NoticeDTO();
+			noticeDTO.setNot_num(rs.getInt("not_num"));
+			noticeDTO.setNot_title(rs.getString("not_title"));
+			noticeDTO.setWriter(rs.getString("writer"));
+			noticeDTO.setContents(rs.getString("contents"));
+			noticeDTO.setReg_date(rs.getDate("reg_date"));
+			noticeDTO.setHit(rs.getInt("hit"));	
+			ar.add(noticeDTO);
+		}
+		DBConnector.disConnect(rs, st, con);
+		
+		return ar;
 	}
 
 	public int getTotalCount(MakeRow makeRow) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		Connection con = DBConnector.getConnect();
+		String sql = "select nvl(count(num), 0) from notice where "+makeRow.getKind()+" like ?";
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setString(1, "%"+makeRow.getSearch()+"%");
+		ResultSet rs = st.executeQuery();
+		rs.next();
+		int totalCount = rs.getInt(1);
+		DBConnector.disConnect(rs, st, con);
+		
+		return totalCount;
 	}
 
 	public int hit(int num) throws Exception {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
