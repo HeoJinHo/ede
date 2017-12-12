6ece991b2c29a0a4d0229dbe6e05e54e883a4cbe
HeoJinHo <33415742+HeoJinHo@users.noreply.github.com>
2017-12-12 20:15:41 +0900

    17.12.12 20:15

--- src/com/ede/member/MemberUpdateService.java
+++ src/com/ede/member/MemberUpdateService.java
@@ -13,7 +13,6 @@ public class MemberUpdateService implements Action {
 		
 		ActionFoward actionFoward = new ActionFoward();
 		String method = request.getMethod();
-		System.out.println(method);
 		MemberDAO memberDAO = new MemberDAO();
 		MemberDTO memberDTO = (MemberDTO)request.getSession().getAttribute("member");
 		if(method.equals("POST")) {
@@ -24,39 +23,22 @@ public class MemberUpdateService implements Action {
 			} catch (Exception e) {
 				// TODO: handle exception
 			}
-			System.out.println(1);
 			if(result>0) {
-				System.out.println(2);
 				actionFoward.setCheck(false);
-				System.out.println(3);
 				actionFoward.setPath("../index.jsp");
-				System.out.println(4);
 			}else{
-				System.out.println(5);
 				request.setAttribute("message", "Fail");
-				System.out.println(6);
 				request.setAttribute("path", "./memberMyPage.member");
-				System.out.println(7);
 				actionFoward.setCheck(true);
-				System.out.println(8);
 				actionFoward.setPath("../WEB-INF/view/common/result.jsp");
-				System.out.println(9);
 			}
 		}else {		
-				System.out.println(10);
-				
-				System.out.println(11);
 				request.setAttribute("member2", "member");
-				System.out.println(12);
 				actionFoward.setPath("../WEB-INF/view/member/memberUpdateForm.jsp");
 			if(memberDTO==null) {
-				System.out.println(13);
 				request.setAttribute("message", "Fail");
-				System.out.println(14);
 				request.setAttribute("path", "./memberMyPage.member");
-				System.out.println(15);
 				actionFoward.setPath("../WEB-INF/view/common/result.jsp");
-				System.out.println(16);
 			}
 			actionFoward.setCheck(true);
 		
